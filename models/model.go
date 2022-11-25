f7370d74575b1959caf8dbe5b4ff2757a9f60217
Caleb Tracey <caleb.tracey@gmail.com>
2022-11-25 11:34:33 -0500

    added PSQLLeaderboardData struct

--- models/model.go
+++ models/model.go
@@ -20,3 +20,26 @@ type TeamLeaderboardData struct {
 	PointsDiff        string `json:"pointsDiff,omitempty"`
 	Points            string `json:"points,omitempty"`
 }
+
+type PSQLLeaderboardDataList []PSQLLeaderboardData
+
+type PSQLLeaderboardData struct {
+	CompId            string `json:"comp_id,omitempty"`
+	CompName          string `json:"comp_name,omitempty"`
+	TeamId            string `json:"team_id,omitempty"`
+	TeamName          string `json:"team_name,omitempty"`
+	GamesPlayed       string `json:"games_played,omitempty"`
+	WinCount          string `json:"win_count,omitempty"`
+	DrawCount         string `json:"draw_count,omitempty"`
+	LossCount         string `json:"loss_count,omitempty"`
+	Bye               string `json:"bye,omitempty"`
+	PointsFor         string `json:"points_for,omitempty"`
+	PointsAgainst     string `json:"points_against,omitempty"`
+	TriesFor          string `json:"tries_for,omitempty"`
+	TriesAgainst      string `json:"tries_against,omitempty"`
+	BonusPointsTry    string `json:"bonus_points_try,omitempty"`
+	BonusPointsLosing string `json:"bonus_points_losing,omitempty"`
+	BonusPoints       string `json:"bonus_points,omitempty"`
+	PointsDiff        string `json:"points_diff,omitempty"`
+	Points            string `json:"points,omitempty"`
+}
